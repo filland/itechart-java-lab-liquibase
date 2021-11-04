87085f9125ecfc6182c4c146d9d97d7cd987aa09
filland <kubalexei@gmail.com>
2021-11-04 22:34:19 +0300

    ref: move controller and remove creds for db

--- src/main/java/com/itechart/api/UserController.java
+++ src/main/java/com/itechart/api/UserController.java
@@ -1,5 +1,7 @@
-package com.itechart.database;
+package com.itechart.api;
 
+import com.itechart.database.User;
+import com.itechart.database.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
